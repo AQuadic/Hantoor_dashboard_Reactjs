31485a04f9e848dbafc6afc0701807edbbcc9c67
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-23 10:41:41 +0300

    adds testing route

--- src/routes/privateRoutes.tsx
+++ src/routes/privateRoutes.tsx
@@ -1,7 +1,12 @@
 import DashboardPage from "@/pages/DashboardPage";
+import Testing from "@/pages/Testing";
 import { RouteTypes } from "@/types/general/RouteTypes";
 
 export const privateRoutes: RouteTypes[] = [
+  {
+    path: "/testing",
+    element: <Testing />,
+  },
   {
     path: "/",
     element: <DashboardPage />,
