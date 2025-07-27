139c2c2723a00e2af8eba8d347290743d18c8d09
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-27 13:54:15 +0300

    adds brands table

---
+++ src/components/brands/BrandsTable.tsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router";
+import TableDeleteButton from "../general/dashboard/table/TableDeleteButton";
+import ActiveStatus from "../icons/general/ActiveStatus";
+import Delete from "../icons/general/Delete";
+import Edit from "../icons/general/Edit";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+export function BrandsTable() {
+  const brands = [
+    {
+      id: 1,
+      image: "/",
+      name: "محمد احمد",
+      count: 22,
+    },
+    {
+      id: 2,
+      image: "/",
+      name: "محمد احمد",
+      count: 22,
+    },
+    {
+      id: 3,
+      image: "/",
+      name: "محمد احمد",
+      count: 22,
+    },
+  ];
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-right">#</TableHead>
+          <TableHead className="text-right">الصورة</TableHead>
+          <TableHead className="text-right">اسم الماركة</TableHead>
+          <TableHead className="text-right">عدد السيارات</TableHead>
+          <TableHead className="text-right">الحالة</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {brands.map((brand, index) => (
+          <TableRow key={brand.id}>
+            <TableCell>{index + 1}</TableCell>
+            <TableCell>{brand.image}</TableCell>
+            <TableCell>{brand.name}</TableCell>
+            <TableCell className="w-full">{brand.count}</TableCell>
+            <TableCell className="flex gap-[7px] items-center">
+              <ActiveStatus />
+              <Link to="">
+                <Edit />
+              </Link>
+
+              <TableDeleteButton handleDelete={() => {}} />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
--- src/pages/BrandsPage.tsx
+++ src/pages/BrandsPage.tsx
@@ -1,10 +1,12 @@
 import BrandsHeader from "@/components/brands/BrandsHeader";
+import { BrandsTable } from "@/components/brands/BrandsTable";
 import React from "react";
 
 const BrandsPage = () => {
   return (
     <section>
       <BrandsHeader />
+      <BrandsTable />
     </section>
   );
 };
