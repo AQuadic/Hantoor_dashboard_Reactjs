8dc1685e8c5bfa11bafae1d685ae17cfabce267b
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-29 13:29:47 +0300

    test

--- src/components/general/Layout.tsx
+++ src/components/general/Layout.tsx
@@ -12,17 +12,21 @@ const Layout = () => {
       <DashboardSidebar />
       <div className="flex-1 overflow-y-auto">
         <DashboardHeader />
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 50 }}
-            transition={{ duration: 0.4, ease: "easeInOut" }}
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
+        {/* Animation container with relative positioning */}
+        <div className="relative min-h-full">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 50 }}
+              transition={{ duration: 0.4, ease: "easeInOut" }}
+              className="absolute inset-0 w-full"
+            >
+              <Outlet />
+            </motion.div>
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   );
