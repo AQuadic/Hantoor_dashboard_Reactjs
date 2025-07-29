24f6e0d324e3b6ab03a3154a28f8c03f667d8d6e
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-29 10:10:44 +0300

    fixes permissions card

--- src/pages/subordinates/AddPermissionPage.tsx
+++ src/pages/subordinates/AddPermissionPage.tsx
@@ -1,12 +1,83 @@
 import DashboardHeader from "@/components/general/dashboard/DashboardHeader";
+import { Input } from "@heroui/react";
 import React from "react";
 import { useParams } from "react-router";
+import PermissionsCard from "@/components/subordinates/PermissionsCard";
 
 const AddPermissionPage = () => {
   const params = useParams();
   const brandId = params.id;
 
   const isEdit = Boolean(brandId);
+
+  const [panelPermissions, setPanelPermissions] = React.useState<
+    {
+      permission: { titleEn: string; titleAr: string };
+      isSelected: boolean;
+    }[]
+  >([
+    {
+      permission: { titleEn: "Dashboard", titleAr: "لوحة التحكم" },
+      isSelected: true,
+    },
+    {
+      permission: {
+        titleEn: "Number of logged-in users",
+        titleAr: "عدد المستخدمين المسجلين",
+      },
+      isSelected: false,
+    },
+    {
+      permission: {
+        titleEn: "Number of advanced search uses",
+        titleAr: "عدد مرات البحث المتقدم",
+      },
+      isSelected: true,
+    },
+    {
+      permission: {
+        titleEn: "Number of times price details requested",
+        titleAr: "عدد مرات طلب تفاصيل سعر السيارة",
+      },
+      isSelected: true,
+    },
+    {
+      permission: {
+        titleEn: "Number of brands",
+        titleAr: "عدد الماركات",
+      },
+      isSelected: false,
+    },
+    {
+      permission: {
+        titleEn: "Number of cars with discount",
+        titleAr: "عدد السيارات التي تحتوي على خصم",
+      },
+      isSelected: false,
+    },
+    {
+      permission: {
+        titleEn: "Number of cars on offer",
+        titleAr: "عدد السيارات التي تحتوي على عرض",
+      },
+      isSelected: false,
+    },
+    {
+      permission: {
+        titleEn: "Number of dealerships",
+        titleAr: "عدد الوكالات",
+      },
+      isSelected: false,
+    },
+    {
+      permission: {
+        titleEn: "Number of trade-in and financing cars",
+        titleAr: "عدد السيارات التجاري و تقسيط بالتقسيط",
+      },
+      isSelected: false,
+    },
+  ]);
+
   return (
     <section>
       <DashboardHeader
@@ -30,10 +101,25 @@ const AddPermissionPage = () => {
           },
         ]}
       />
-      <div className="flex flex-col gap-8 p-8">
-        <div className="p-8 bg-white rounded-2xl">
-          <h3 className="mb-4 text-lg font-bold">الصورة الشخصية</h3>
-        </div>
+      <div className="flex flex-col gap-8 p-8 pt-0 bg-white rounded-b-2xl">
+        <Input
+          label="اسم الصلاحية"
+          variant="bordered"
+          value={"مدير"}
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+          className="max-w-[680px]"
+        />
+      </div>
+      <div className={"px-8 py-4"}>
+        <PermissionsCard
+          titleAr="لوحة التحكم"
+          titleEn="Control panel"
+          selectedPermissions={panelPermissions}
+          setSelectedPermissions={(newPermissions) =>
+            setPanelPermissions(newPermissions)
+          }
+        />
       </div>
     </section>
   );
---
+++ src/types/PermissionTypes.ts
@@ -0,0 +1,9 @@
+export interface PermissionTypes {
+  titleEn: string;
+  titleAr: string;
+}
+
+export interface PermissionStateTypes {
+  permission: PermissionTypes;
+  isSelected: boolean;
+}
