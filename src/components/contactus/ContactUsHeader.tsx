af79b31807d92c7001a2990a63782045cb0047a2
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-29 15:15:17 +0300

    Adds placeholder to searchbars

--- src/components/contactus/ContactUsHeader.tsx
+++ src/components/contactus/ContactUsHeader.tsx
@@ -16,7 +16,7 @@ const ContactUsHeader = () => {
 
         <div className="flex flex-wrap items-center gap-2 px-2 md:px-8">
             <div className="flex-1">
-            <SearchBar term={""} setTerm={() => {}} />
+            <SearchBar term={"ابحث بالاسم"} setTerm={() => {}} />
             </div>
             <div className="flex-1">
             <DashboardDatePicker />
--- src/components/faqs/FAQsHeader.tsx
+++ src/components/faqs/FAQsHeader.tsx
@@ -19,7 +19,7 @@ const FAQsHeader = () => {
 
         <div className="flex flex-wrap items-center gap-2 px-2 md:px-8">
             <div className="flex-1">
-            <SearchBar term={""} setTerm={() => {}} />
+            <SearchBar term={"ابحث بالاسم"} setTerm={() => {}} />
             </div>
             <div className="flex-1">
             <DashboardDatePicker />
