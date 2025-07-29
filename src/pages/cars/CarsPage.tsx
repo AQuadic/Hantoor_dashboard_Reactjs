cc83cabdad4daea26a24af46d708b24935cc5f43
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-29 17:10:49 +0300

    add cars table

--- src/pages/cars/CarsPage.tsx
+++ src/pages/cars/CarsPage.tsx
@@ -1,10 +1,14 @@
 import CarsHeader from '@/components/cars/CarsHeader'
+import CarsTable from '@/components/cars/CarsTable'
 import React from 'react'
 
 const CarsPage = () => {
     return (
         <div>
             <CarsHeader />
+            <div className="px-2 md:px-8">
+                <CarsTable />
+            </div>
         </div>
     )
 }
