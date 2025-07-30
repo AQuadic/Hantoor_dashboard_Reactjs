972842a7d03f68a560e7ddc345d101419fe1fffc
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-30 16:48:19 +0300

    fix icons

--- src/components/icons/general/DateSelectorIcon.tsx
+++ src/components/icons/general/DateSelectorIcon.tsx
@@ -13,43 +13,43 @@ const DateSelectorIcon = () => {
       <path
         d="M21.25 9.875H10.75C9.7835 9.875 9 10.6585 9 11.625V22.125C9 23.0915 9.7835 23.875 10.75 23.875H21.25C22.2165 23.875 23 23.0915 23 22.125V11.625C23 10.6585 22.2165 9.875 21.25 9.875Z"
         stroke="#2A32F8"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M19.5 8.125V11.625"
         stroke="#2A32F8"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M12.5 8.125V11.625"
         stroke="#2A32F8"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M9 15.125H23"
         stroke="#2A32F8"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M15.125 18.625H16"
         stroke="#2A32F8"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M16 18.625V21.25"
         stroke="#2A32F8"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
     </svg>
