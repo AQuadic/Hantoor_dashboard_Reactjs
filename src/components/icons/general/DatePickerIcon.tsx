9a89dcd183190a6f9083c00bffaae51657187941
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-30 13:21:57 +0300

    adds pricing section

---
+++ src/components/icons/general/DatePickerIcon.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const DatePickerIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="22"
+      viewBox="0 0 20 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.75 3.125H3.25C2.00736 3.125 1 4.13236 1 5.375V18.875C1 20.1176 2.00736 21.125 3.25 21.125H16.75C17.9926 21.125 19 20.1176 19 18.875V5.375C19 4.13236 17.9926 3.125 16.75 3.125Z"
+        stroke="#2A32F8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.5 0.875V5.375"
+        stroke="#2A32F8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5.5 0.875V5.375"
+        stroke="#2A32F8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M1 9.875H19"
+        stroke="#2A32F8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.875 14.375H10"
+        stroke="#2A32F8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10 14.375V17.75"
+        stroke="#2A32F8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default DatePickerIcon;
--- src/pages/cars/AddCars.tsx
+++ src/pages/cars/AddCars.tsx
@@ -1,18 +1,20 @@
-import AddCarsHeader from '@/components/cars/addcars/AddCarsHeader'
-import CarDetails from '@/components/cars/addcars/CarDetails'
-import PhotosAndVideos from '@/components/cars/addcars/PhotosAndVideos'
-import React from 'react'
+import AddCarsHeader from "@/components/cars/addcars/AddCarsHeader";
+import CarDetails from "@/components/cars/addcars/CarDetails";
+import PhotosAndVideos from "@/components/cars/addcars/PhotosAndVideos";
+import React from "react";
+import CarPrices from "@/components/cars/addcars/CarPrices";
 
 const AddCars = () => {
-    return (
-        <div>
-            <AddCarsHeader />
-            <div className='md:px-8 px-2'>
-                <PhotosAndVideos />
-                <CarDetails />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <AddCarsHeader />
+      <div className="md:px-8 px-2">
+        <PhotosAndVideos />
+        <CarDetails />
+        <CarPrices />
+      </div>
+    </div>
+  );
+};
 
-export default AddCars
+export default AddCars;
