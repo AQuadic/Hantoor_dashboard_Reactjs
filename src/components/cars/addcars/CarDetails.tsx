680e62fa338ea11cdd3df2ad51a68ab1235a92e1
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-30 11:28:08 +0300

    adds selects

--- src/components/cars/addcars/CarDetails.tsx
+++ src/components/cars/addcars/CarDetails.tsx
@@ -1,32 +1,133 @@
-import { Input } from '@heroui/react'
-import React from 'react'
+import { Input, Select, SelectItem } from "@heroui/react";
+import React from "react";
 
 const CarDetails = () => {
-    return (
-        <div className="bg-white mt-3 rounded-[15px] py-[19px] px-[29px]">
-            <h1 className="text-lg text-[#2A32F8] font-bold mb-2">بيانات السيارة</h1>
-            <div className="flex flex-col md:flex-row gap-[15px]">
-                <div className="w-full">
-                <Input
-                    label="اسم مركز الصيانة ( باللغة العربية )"
-                    variant="bordered"
-                    placeholder=" المركز الدولي لصيانة السيارات"
-                    classNames={{ label: "mb-2 text-base" }}
-                    size="lg"
-                />
-                </div>
-                <div className="w-full">
-                <Input
-                    label="اسم مركز الصيانة ( باللغة الانجليزية )"
-                    variant="bordered"
-                    placeholder="اكتب هنا"
-                    classNames={{ label: "mb-2 text-base" }}
-                    size="lg"
-                />
-            </div>
+  return (
+    <div className="bg-white mt-3 rounded-[15px] py-[19px] px-[29px]">
+      <h1 className="text-lg text-[#2A32F8] font-bold mb-2">بيانات السيارة</h1>
+      <div className="flex flex-col md:flex-row gap-[15px]">
+        <div className="w-full">
+          <Input
+            label="اسم السيارة (باللغة العربية)"
+            variant="bordered"
+            placeholder="اكتب هنا"
+            classNames={{ label: "mb-2 text-base" }}
+            size="lg"
+          />
         </div>
+        <div className="w-full">
+          <Input
+            label="اسم السيارة (باللغة الانجليزية)"
+            variant="bordered"
+            placeholder="اكتب هنا"
+            classNames={{ label: "mb-2 text-base" }}
+            size="lg"
+          />
         </div>
-    )
-}
+      </div>
+      <div className="mt-4 grid grid-cols-2 sm:grid-cols-4 gap-[15px]">
+        <Select
+          label="البلد"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="الماركة"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="الوكيل"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="الموديل"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="نوع الهيكل"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="النوع"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="الفئة"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="منشأ الماركة"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="عدد المقاعد"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="نوع الماكينة"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>{" "}
+        <Select
+          label="حجم الماكينة"
+          variant="bordered"
+          placeholder=" اختر"
+          classNames={{ label: "mb-2 text-base" }}
+          size="lg"
+        >
+          <SelectItem>test </SelectItem>
+        </Select>
+      </div>
+    </div>
+  );
+};
 
-export default CarDetails
+export default CarDetails;
