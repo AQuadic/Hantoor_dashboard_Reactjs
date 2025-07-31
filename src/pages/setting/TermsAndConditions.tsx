e62b3ff7ad369967426f3595fa39c5ff361d21cc
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-31 14:43:39 +0300

    add terms and conditions pagenation

--- src/pages/setting/TermsAndConditions.tsx
+++ src/pages/setting/TermsAndConditions.tsx
@@ -1,3 +1,4 @@
+import TablePagination from '@/components/general/dashboard/table/TablePagination'
 import TermsHeader from '@/components/termsandconditions/TermsHeader'
 import TermsTable from '@/components/termsandconditions/TermsTable'
 import React from 'react'
@@ -7,6 +8,15 @@ const TermsAndConditions = () => {
         <div className='md:px-8 px-2'>
             <TermsHeader />
             <TermsTable />
+            <TablePagination
+                currentPage={0}
+                setCurrentPage={function (): void {
+                    throw new Error("Function not implemented.");
+                }}
+                totalPages={20}
+                totalItems={20}
+                itemsPerPage={5}
+                />
         </div>
     )
 }
