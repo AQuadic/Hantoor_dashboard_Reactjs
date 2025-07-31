7870f14803ac3342c1d9dfe203d7f4f103d57e85
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-31 17:01:26 +0300

    change text

--- src/components/cars/addcars/CarAdvertisingImages.tsx
+++ src/components/cars/addcars/CarAdvertisingImages.tsx
@@ -7,7 +7,7 @@ const CarAdvertisingImages = ( ) => {
   return (
     <div className="bg-white mt-3 rounded-[15px] py-[19px] px-[29px] ">
       <h1 className="text-lg text-[#2A32F8] font-bold mb-4">الصور الاعلانية في اخر الصفحة</h1>
-        <ImageInput image={profileImage} setImage={setProfileImage} placeholderText="اضافة GIF"   width={378}
+        <ImageInput image={profileImage} setImage={setProfileImage}  width={378}
             height={169}/>
     
     </div>
