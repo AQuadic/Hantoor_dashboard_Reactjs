102f1922ac4b11449b1de2dccb941ccaaa908e9b
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-31 14:44:49 +0300

    add features pagination

--- src/pages/features/FeaturesPage.tsx
+++ src/pages/features/FeaturesPage.tsx
@@ -1,11 +1,21 @@
 import FeaturesHeader from "@/components/features/FeaturesHeader"
 import FeaturesTable from "@/components/features/FeaturesTable"
+import TablePagination from "@/components/general/dashboard/table/TablePagination";
 
 const FeaturesPage = () => {
     return (
         <div className="md:px-8 px-2">
             <FeaturesHeader />
             <FeaturesTable />
+            <TablePagination
+                currentPage={0}
+                setCurrentPage={function (): void {
+                    throw new Error("Function not implemented.");
+                }}
+                totalPages={10}
+                totalItems={10}
+                itemsPerPage={5}
+                />
         </div>
     )
 }
