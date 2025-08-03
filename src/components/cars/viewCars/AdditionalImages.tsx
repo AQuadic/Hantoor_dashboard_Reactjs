6fa1f6c2c32e6b872c9153462d080701b1c50b43
ahmed <ahmedabozaid.dev@gmail.com>
2025-08-03 13:12:01 +0300

    adds additional images

--- src/components/cars/viewCars/AdditionalImages.tsx
+++ src/components/cars/viewCars/AdditionalImages.tsx
@@ -1,5 +1,41 @@
 const AdditionalImages = () => (
-  <div className="p-8 text-center">Additional Images Placeholder</div>
+  <div className="p-8 text-center flex gap-4 flex-wrap">
+    <img
+      src="/images/additionalImages.png"
+      alt="Additional Images Placeholder"
+      className="w-[211px] h-[163]"
+    />
+    <img
+      src="/images/additionalImages.png"
+      alt="Additional Images Placeholder"
+      className="w-[211px] h-[163]"
+    />
+    <img
+      src="/images/additionalImages.png"
+      alt="Additional Images Placeholder"
+      className="w-[211px] h-[163]"
+    />
+    <img
+      src="/images/additionalImages.png"
+      alt="Additional Images Placeholder"
+      className="w-[211px] h-[163]"
+    />
+    <img
+      src="/images/additionalImages.png"
+      alt="Additional Images Placeholder"
+      className="w-[211px] h-[163]"
+    />
+    <img
+      src="/images/additionalImages.png"
+      alt="Additional Images Placeholder"
+      className="w-[211px] h-[163]"
+    />
+    <img
+      src="/images/additionalImages.png"
+      alt="Additional Images Placeholder"
+      className="w-[211px] h-[163]"
+    />
+  </div>
 );
 
 export default AdditionalImages;
