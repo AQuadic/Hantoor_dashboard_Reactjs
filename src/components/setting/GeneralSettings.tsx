e96fa61a41b84fc2493cf8177724ebeb55680407
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-03 15:07:47 +0300

    change textarea

--- src/components/setting/GeneralSettings.tsx
+++ src/components/setting/GeneralSettings.tsx
@@ -2,7 +2,6 @@ import { Input } from '@heroui/react'
 import React from 'react'
 import DashboardButton from '../general/dashboard/DashboardButton'
 import ImageInput from '../general/ImageInput'
-import {Textarea} from "@heroui/input";
 import { useTranslation } from 'react-i18next';
 
 const GeneralSettings = () => {
@@ -36,12 +35,20 @@ const GeneralSettings = () => {
             <div className='h-full bg-[#FFFFFF] rounded-[15px] mx-8 px-[29px] py-5 mt-[11px]'>
                 <h2 className='text-[#2A32F8] text-[17px] font-bold mb-3'>{t('textFeatures')}</h2>
                 <div className='flex md:flex-row flex-col items-center gap-[15px]'>
-                    <div className='w-full'>
-                        <Textarea className="mb-3" label={t('arText')} placeholder={t('writeHere')} style={{ backgroundColor: '#f0f0f0' }}  />
+                    <div className='w-full relative'>
+                        <h2 className='absolute top-[11px] rtl:right-5 text-[#000000] text-[15px] font-normal'>{t('arText')}</h2>
+                        <textarea
+                        className='w-full h-32 border border-[#E2E2E2] rounded-[12px] px-5 py-8'
+                        placeholder={t('writeHere')}
+                        ></textarea>
                         <DashboardButton titleAr="حفظ" titleEn="Save" />
                     </div>
-                    <div className='w-full'>
-                        <Textarea className="mb-3" label={t('enText')} placeholder={t('writeHere')} />
+                    <div className='w-full relative'>
+                        <h2 className='absolute top-[11px] rtl:right-5 text-[#000000] text-[15px] font-normal'>{t('enText')}</h2>
+                        <textarea
+                        className='w-full h-32 border border-[#E2E2E2] rounded-[12px] px-5 py-8'
+                        placeholder={t('writeHere')}
+                        ></textarea>
                         <DashboardButton titleAr="حفظ" titleEn="Save" />
                     </div>
                 </div>
@@ -51,12 +58,20 @@ const GeneralSettings = () => {
             <div className='h-full bg-[#FFFFFF] rounded-[15px] mx-8 px-[29px] py-5 mt-[11px]'>
                 <h2 className='text-[#2A32F8] text-[17px] font-bold mb-3'>{t('advancedSearch')}</h2>
                 <div className='flex md:flex-row flex-col items-center gap-[15px]'>
-                    <div className='w-full'>
-                        <Textarea className="mb-3" label={t('arText')} placeholder={t('writeHere')} />
+                    <div className='w-full relative'>
+                        <h2 className='absolute top-[11px] rtl:right-5 text-[#000000] text-[15px] font-normal'>{t('arText')}</h2>
+                        <textarea
+                        className='w-full h-32 border border-[#E2E2E2] rounded-[12px] px-5 py-8'
+                        placeholder={t('writeHere')}
+                        ></textarea>
                         <DashboardButton titleAr="حفظ" titleEn="Save" />
                     </div>
-                    <div className='w-full'>
-                        <Textarea className="mb-3" label={t('enText')} placeholder={t('writeHere')} />
+                    <div className='w-full relative'>
+                        <h2 className='absolute top-[11px] rtl:right-5 text-[#000000] text-[15px] font-normal'>{t('enText')}</h2>
+                        <textarea
+                        className='w-full h-32 border border-[#E2E2E2] rounded-[12px] px-5 py-8'
+                        placeholder={t('writeHere')}
+                        ></textarea>
                         <DashboardButton titleAr="حفظ" titleEn="Save" />
                     </div>
                 </div>
@@ -66,12 +81,20 @@ const GeneralSettings = () => {
             <div className='h-full bg-[#FFFFFF] rounded-[15px] mx-8 px-[29px] py-5 mt-[11px]'>
                 <h2 className='text-[#2A32F8] text-[17px] font-bold mb-3'>{t('financingText')}</h2>
                 <div className='flex md:flex-row flex-col items-center gap-[15px]'>
-                    <div className='w-full'>
-                        <Textarea className="mb-3" label={t('arText')} placeholder={t('writeHere')} />
+                    <div className='w-full relative'>
+                        <h2 className='absolute top-[11px] rtl:right-5 text-[#000000] text-[15px] font-normal'>{t('arText')}</h2>
+                        <textarea
+                        className='w-full h-32 border border-[#E2E2E2] rounded-[12px] px-5 py-8'
+                        placeholder={t('writeHere')}
+                        ></textarea>
                         <DashboardButton titleAr="حفظ" titleEn="Save" />
                     </div>
-                    <div className='w-full'>
-                        <Textarea className="mb-3" label={t('enText')} placeholder={t('writeHere')} />
+                    <div className='w-full relative'>
+                        <h2 className='absolute top-[11px] rtl:right-5 text-[#000000] text-[15px] font-normal'>{t('enText')}</h2>
+                        <textarea
+                        className='w-full h-32 border border-[#E2E2E2] rounded-[12px] px-5 py-8'
+                        placeholder={t('writeHere')}
+                        ></textarea>
                         <DashboardButton titleAr="حفظ" titleEn="Save" />
                     </div>
                 </div>
