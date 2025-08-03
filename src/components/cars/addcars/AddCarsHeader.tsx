2fdc6af0ab76b1bbb85e6b58c8ffc2a1bee98f5c
ahmed <ahmedabozaid.dev@gmail.com>
2025-08-03 10:18:56 +0300

    fixes cars header links

--- src/components/cars/addcars/AddCarsHeader.tsx
+++ src/components/cars/addcars/AddCarsHeader.tsx
@@ -1,20 +1,19 @@
-import DashboardHeader from "@/components/general/dashboard/DashboardHeader"
-
+import DashboardHeader from "@/components/general/dashboard/DashboardHeader";
 
 const AddCarsHeader = () => {
-    return (
-        <div className="pt-2 pb-6 bg-white ">
-        <DashboardHeader
-            titleAr="اضافة سيارة جديدة"
-            titleEn="Add new car"
-            items={[
-            { titleAr: "لوحة التحكم", titleEn: "Dashboard", link: "/" },
-            { titleAr: "السيارات", titleEn: "Cars" },
-            { titleAr: "اضافة سيارة جديدة", titleEn: "Add new car" },
-            ]}
-        />
-        </div>
-    )
-}
+  return (
+    <div className="pt-2 pb-6 bg-white ">
+      <DashboardHeader
+        titleAr="اضافة سيارة جديدة"
+        titleEn="Add new car"
+        items={[
+          { titleAr: "لوحة التحكم", titleEn: "Dashboard", link: "/" },
+          { titleAr: "السيارات", titleEn: "Cars", link: "/cars" },
+          { titleAr: "اضافة سيارة جديدة", titleEn: "Add new car" },
+        ]}
+      />
+    </div>
+  );
+};
 
-export default AddCarsHeader
+export default AddCarsHeader;
