eac7590afe47c9dbfac5fab267a1b08fc249f208
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-11 12:29:07 +0300

    set up react query

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,15 +1,23 @@
 import { HeroUIProvider } from "@heroui/react";
 import React from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 const Providers: React.FC<ProvidersProps> = ({ children }) => {
   return (
-    <div>
-      <HeroUIProvider>{children}</HeroUIProvider>
-    </div>
+    <HeroUIProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </HeroUIProvider>
   );
 };
 
