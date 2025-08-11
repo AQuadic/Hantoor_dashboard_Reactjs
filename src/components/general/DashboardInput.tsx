4f0e8ffb689048aa4b2dfaf6e152ab3071fc2054
ahmed <ahmedabozaid.dev@gmail.com>
2025-08-11 14:04:13 +0300

    fixes input

--- src/components/general/DashboardInput.tsx
+++ src/components/general/DashboardInput.tsx
@@ -14,7 +14,7 @@ const DashboardInput: React.FC<DashboardInputProps> = ({
   placeholder,
 }) => {
   return (
-    <div className="flex flex-col border px-5 py-2 rounded-2xl  ">
+    <div className="flex flex-col border px-5 py-2 rounded-2xl max-h-[64px]  ">
       <label>{label}</label>
       <input
         type="text"
