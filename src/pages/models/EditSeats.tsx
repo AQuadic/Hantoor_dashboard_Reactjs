4d0ee6fcf44dc2ebdf01799a9707c18aaf59169e
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-12 11:16:03 +0300

    fix edit seats form - responsive

--- src/pages/models/EditSeats.tsx
+++ src/pages/models/EditSeats.tsx
@@ -58,9 +58,9 @@ const EditSeats = () => {
           },
         ]}
       />
-      <div className="flex flex-col gap-8 p-8">
-        <div className="flex flex-col gap-4 p-8 bg-white rounded-2xl">
-          <div className="flex gap-4">
+      <div className="flex flex-col gap-8 md:p-8 p-2">
+        <div className="flex flex-col gap-4 lg:p-8 p-2 bg-white rounded-2xl">
+          <div className="flex md:flex-row flex-col gap-4">
             <div className="flex-1">
               <DashboardInput
                 label={t("arSeatsNumbers")}
