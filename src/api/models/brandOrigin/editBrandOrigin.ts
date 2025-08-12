cf17d4653f2f240094d869f5ea1fe77b896edd47
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-12 10:30:37 +0300

    edit brand origin

--- src/api/models/brandOrigin/editBrandOrigin.ts
+++ src/api/models/brandOrigin/editBrandOrigin.ts
@@ -1,6 +1,12 @@
 import { axios } from "@/lib/axios";
 
-export const updateBrandOrigin = async (id: number, data: { name: { ar: string; en: string } }) => {
+export const updateBrandOrigin = async (
+  id: number,
+  data: {
+    name?: { ar?: string; en?: string };
+    is_active?: boolean;
+  }
+) => {
     const res = await axios.patch(`/admin/brand-origin/${id}`, data);
     return res.data;
 };
