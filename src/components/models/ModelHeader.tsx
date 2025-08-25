320488c3a64d74e786add29870d361e006e968c1
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-25 12:13:21 +0300

    change search title

--- src/components/models/ModelHeader.tsx
+++ src/components/models/ModelHeader.tsx
@@ -126,10 +126,11 @@ const filtersData = [
       <div className="flex flex-wrap items-center gap-2 px-2 md:px-8">
         <div className="flex-1">
             <SearchBar
-            termAr={"ابحث بالاسم"}
-            termEn={"Search by name"}
+            termAr={""}
+            termEn={""}
             setTermAr={setSearch} 
             setTermEn={setSearch}
+            placeholder={t("searchByName")}
             />
         </div>
         <div className="flex-1">
