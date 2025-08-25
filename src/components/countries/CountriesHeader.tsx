76787e60610f21ab6cdf6b1161f74b41c1bf88b4
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-25 14:29:18 +0300

    change countries search title

--- src/components/countries/CountriesHeader.tsx
+++ src/components/countries/CountriesHeader.tsx
@@ -3,8 +3,10 @@ import DashboardButton from "../general/dashboard/DashboardButton";
 import DashboardDatePicker from "../general/dashboard/DashboardDatePicker";
 import DashboardHeader from "../general/dashboard/DashboardHeader";
 import SearchBar from "../general/dashboard/SearchBar";
+import { useTranslation } from "react-i18next";
 
 const CountriesHeader = () => {
+    const { t } = useTranslation("country");
     return (
         <div className="pt-0 pb-2 bg-white border-b border-[#E1E1E1]">
         <DashboardHeader
@@ -19,10 +21,11 @@ const CountriesHeader = () => {
         <div className="flex flex-wrap items-center gap-2 px-2 md:px-8">
             <div className="flex-1">
                 <SearchBar
-                    termAr={"ابحث بالاسم"}
-                    termEn={"Search by name"}
+                    termAr={""}
+                    termEn={" "}
                     setTermAr={() => {}} 
                     setTermEn={() => {}} 
+                    placeholder={t('searchByName')}
                 />
             </div>
             <div className="flex-1">
