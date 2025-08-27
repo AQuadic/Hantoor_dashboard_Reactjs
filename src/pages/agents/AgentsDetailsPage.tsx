56b70db039ede1cc2f59ddaf3882b795afcaa12b
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-27 10:34:18 +0300

    remove agent details animation

--- src/pages/agents/AgentsDetailsPage.tsx
+++ src/pages/agents/AgentsDetailsPage.tsx
@@ -1,6 +1,5 @@
 import AgentsHeader from '@/components/agents/AgentsHeader'
 import React, { useState } from 'react'
-import { AnimatePresence, motion } from "framer-motion";
 import MaintenanceCentersTable from '@/components/agents/MaintenanceCentersTable';
 import SalesShowroomsTable from '@/components/agents/SalesShowroomsTable';
 
@@ -14,29 +13,11 @@ const AgentsDetailsPage = () => {
                 setSelectedFilter={setSelectedFilter}
             />
             <div className="px-2 md:px-8 relative min-h-[300px]">
-                <AnimatePresence mode="wait">
                 {selectedFilter === "Maintenance centers" ? (
-                    <motion.div
-                    key="Maintenance"
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: -20 }}
-                    transition={{ duration: 0.3 }}
-                    >
                     <MaintenanceCentersTable />
-                    </motion.div>
                 ) : (
-                    <motion.div
-                    key="Sales Showrooms"
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: -20 }}
-                    transition={{ duration: 0.3 }}
-                    >
                     <SalesShowroomsTable />
-                    </motion.div>
                 )}
-                </AnimatePresence>
             </div>
         </div>
     )
