79635582ff3abadef9fb0d85c20cbe3a84795ec7
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-09-02 14:16:02 +0300

    change arrow

--- src/components/general/DashboardSidebar.tsx
+++ src/components/general/DashboardSidebar.tsx
@@ -138,14 +138,14 @@ const DashboardSidebar = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M9 5l7 7-7 7"
+                d="M15 19l-7-7 7-7"
               />
             ) : (
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M15 19l-7-7 7-7"
+                d="M9 5l7 7-7 7"
               />
             )}
           </motion.svg>
