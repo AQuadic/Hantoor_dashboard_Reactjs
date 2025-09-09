d0f32dc45010e8851064436bdffa43df7fdce772
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-09-09 12:56:06 +0300

    integrate additional images

--- src/components/cars/viewCars/AdditionalImages.tsx
+++ src/components/cars/viewCars/AdditionalImages.tsx
@@ -1,41 +1,31 @@
-const AdditionalImages = () => (
-  <div className="p-8 text-center flex gap-4 flex-wrap ">
-    <img
-      src="/images/additionalImages.png"
-      alt="Additional Images Placeholder"
-      className="w-[211px] h-[163]"
-    />
-    <img
-      src="/images/additionalImages.png"
-      alt="Additional Images Placeholder"
-      className="w-[211px] h-[163]"
-    />
-    <img
-      src="/images/additionalImages.png"
-      alt="Additional Images Placeholder"
-      className="w-[211px] h-[163]"
-    />
-    <img
-      src="/images/additionalImages.png"
-      alt="Additional Images Placeholder"
-      className="w-[211px] h-[163]"
-    />
-    <img
-      src="/images/additionalImages.png"
-      alt="Additional Images Placeholder"
-      className="w-[211px] h-[163]"
-    />
-    <img
-      src="/images/additionalImages.png"
-      alt="Additional Images Placeholder"
-      className="w-[211px] h-[163]"
-    />
-    <img
-      src="/images/additionalImages.png"
-      alt="Additional Images Placeholder"
-      className="w-[211px] h-[163]"
-    />
-  </div>
+import NoData from "@/components/general/NoData";
+import React from "react";
+
+interface AdditionalImage {
+  id: number;
+  url: string;
+  responsive_urls: string[];
+}
+
+const AdditionalImages = ({ images }: { images: AdditionalImage[] }) => {
+  if (!images || images.length === 0) {
+    return (
+      <NoData />
+    );
+  }
+
+  return (
+    <div className="p-8 flex gap-4 flex-wrap justify-center">
+      {images.map((img) => (
+        <img
+          key={img.id}
+          src={img.url}
+          alt={`Additional Image ${img.id}`}
+          className="w-[211px] h-[163px] object-cover rounded-lg shadow"
+        />
+      ))}
+    </div>
 );
+}
 
 export default AdditionalImages;
