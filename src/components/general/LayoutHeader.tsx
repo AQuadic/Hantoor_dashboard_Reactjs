e5cee101fba8c2d386426793bb1608b2f5ac26be
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-09-16 16:35:42 +0300

    change admin image

--- src/components/general/LayoutHeader.tsx
+++ src/components/general/LayoutHeader.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router";
 import ChangeLanguage from "./ChangeLanguage";
 import { useQuery } from "@tanstack/react-query";
-import defaultProfile from "/images/avatar.svg";
 import { getCurrentAdmin, GetCurrentAdminResponse } from "@/api/profile/getProfile";
 
 const LayoutHeader = () => {
@@ -20,11 +19,17 @@ const LayoutHeader = () => {
   return (
     <header className="sticky left-0 right-0 top-0 z-50 w-full bg-[#F4F4FE]  border-l border-b h-[64px] md:px-14 px-4 flex items-center gap-4 justify-end">
       <Link to="/profile" className="flex items-center gap-2">
-        <img
-          src={admin?.image?.url || defaultProfile}
-          alt="Profile"
-          className="w-10 h-10 rounded-full object-cover"
-        />
+        {admin?.image?.url ? (
+            <img
+              src={admin.image.url}
+              alt={admin.name || "Profile"}
+              className="w-10 h-10 rounded-full object-cover"
+            />
+          ) : (
+            <div className="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center text-white">
+              {admin?.name ? admin.name[0].toUpperCase() : "U"}
+            </div>
+          )}
         <div>
           <p className="flex items-center gap-1">
             <span className="">
