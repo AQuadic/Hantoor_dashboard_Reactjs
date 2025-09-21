6b4027890153c9067256dff8b4ba65e48262d8c6
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-09-21 11:51:20 +0300

    translate data - countries page

--- src/components/countries/CountriesTable.tsx
+++ src/components/countries/CountriesTable.tsx
@@ -100,15 +100,18 @@ const CountriesTable = ({
               </TableCell>
               <TableCell style={{ textAlign: "center" }}>0</TableCell>
               <TableCell className="w-full">
-                {new Date(country.created_at).toLocaleString("en-GB", {
-                  day: "2-digit",
-                  month: "2-digit",
-                  year: "numeric",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  second: "2-digit",
-                  hour12: false,
-                })}
+                {new Date(country.created_at).toLocaleString(
+                  i18n.language === "ar" ? "ar-EG" : "en-GB",
+                  {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    hour12: false,
+                  }
+                )}
               </TableCell>
               <TableCell className="flex gap-[7px] items-center">
                 <Switch
