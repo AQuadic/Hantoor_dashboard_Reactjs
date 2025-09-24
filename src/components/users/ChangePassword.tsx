417ef0e36d3ffe1e6771b75ce9a7495ec17cdd63
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-09-24 13:46:41 +0300

    fix change password - users

--- src/components/users/ChangePassword.tsx
+++ src/components/users/ChangePassword.tsx
@@ -6,25 +6,28 @@ import { Eye, EyeOff } from "lucide-react";
 import { changePassword } from "@/api/password/changePassword";
 import toast from "react-hot-toast";
 import { useAuthStore } from "@/store/useAuthStore";
+import { useNavigate } from "react-router";
 
 const ChangePassword = () => {
   const { t } = useTranslation("users");
   const { user } = useAuthStore();
 
+  const [currentPassword, setCurrentPassword] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const [loading, setLoading] = useState(false);
-
+  const navigate = useNavigate();
   const handleSubmit = async () => {
-    if (!user?.id) {
-      toast.error("User not authenticated");
-      return;
-    }
-    if (!password || !passwordConfirmation) {
+    // if (!user?.id) {
+    //   toast.error("User not authenticated");
+    //   return;
+    // }
+    if (!currentPassword || !password || !passwordConfirmation) {
       toast.error(t("allFieldsRequired") || "All fields are required");
       return;
     }
@@ -37,11 +40,16 @@ const ChangePassword = () => {
       setLoading(true);
 
       const response = await changePassword({
+        current_password: currentPassword,
         password,
         password_confirmation: passwordConfirmation,
       });
 
       toast.success(response.message || t("successMessage"));
+      navigate('/users')
+      setCurrentPassword("");
+      setPassword("");
+      setPasswordConfirmation("");
     } catch (error: any) {
       toast.error(error?.response?.data?.message || t("errorMessage"));
     } finally {
@@ -67,12 +75,30 @@ const ChangePassword = () => {
 
       <div className=" bg-white mt-3 rounded-[15px] py-[19px] px-[29px] mx-8">
         <div className="grid md:grid-cols-2 grid-cols-1 items-center gap-[15px]">
+          {/* Current Password */}
+          <div className="relative w-full">
+            <input
+              type={showCurrentPassword ? "text" : "password"}
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              className="w-full h-[64px] border border-[#E2E2E2] rounded-[12px] px-4 pt-4"
+              placeholder="********************"
+            />
+            <h2 className="text-[#000000] text-[15px] absolute top-2 rtl:right-4 ltr:left-4">
+              {t("currentPassword")}
+            </h2>
+            <div
+              className="absolute top-7 rtl:left-5 ltr:right-5 cursor-pointer"
+              onClick={() => setShowCurrentPassword((prev) => !prev)}
+            >
+              {showCurrentPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+            </div>
+          </div>
+
           {/* New Password */}
           <div className="relative w-full">
             <input
               type={showPassword ? "text" : "password"}
-              name="password"
-              id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="w-full h-[64px] border border-[#E2E2E2] rounded-[12px] px-4 pt-4"
@@ -93,8 +119,6 @@ const ChangePassword = () => {
           <div className="relative w-full">
             <input
               type={showConfirmPassword ? "text" : "password"}
-              name="password_confirmation"
-              id="password_confirmation"
               value={passwordConfirmation}
               onChange={(e) => setPasswordConfirmation(e.target.value)}
               className="w-full h-[64px] border border-[#E2E2E2] rounded-[12px] px-4 pt-4"
