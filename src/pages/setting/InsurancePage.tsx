c506ba93c3cf7bcfbca098fa4467f4a76f5f1e71
ahmed <ahmedabozaid.dev@gmail.com>
2025-10-28 14:48:56 +0300

    fixes request financing page

--- src/pages/setting/InsurancePage.tsx
+++ src/pages/setting/InsurancePage.tsx
@@ -1,23 +1,21 @@
-import { useState } from "react";
-import InsuranceHeader from '@/components/setting/insuranceprice/InsuranceHeader';
-import InsuranceTable from '@/components/setting/insuranceprice/InsuranceTable';
-import React from 'react'
+import React, { useState } from "react";
+import InsuranceHeader from "@/components/setting/insuranceprice/InsuranceHeader";
+import InsuranceTable from "@/components/setting/insuranceprice/InsuranceTable";
 
 const InsurancePage = () => {
   const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm] = useState("");
 
-    return (
-        <div className='px-2 md:px-8'>
-        <InsuranceHeader
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            selectedCountry={selectedCountry}
-            setSelectedCountry={setSelectedCountry}
-        />
-        <InsuranceTable selectedCountry={selectedCountry} />
-        </div>
-    )
-}
+  return (
+    <div className="px-2 md:px-8">
+      <InsuranceHeader
+        searchTerm={searchTerm}
+        selectedCountry={selectedCountry}
+        setSelectedCountry={setSelectedCountry}
+      />
+      <InsuranceTable selectedCountry={selectedCountry} />
+    </div>
+  );
+};
 
-export default InsurancePage
+export default InsurancePage;
